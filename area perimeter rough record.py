a9ebfcef1f7abbcbf84065b3032e5a1797e21f96
yadhu krishna pm <txdrona@gmail.com>
2025-07-17 09:22:10 +0530

    area and perimeter
    appo sherii

---
+++ area perimeter rough record.py
@@ -0,0 +1,15 @@
+Python 3.13.4 (tags/v3.13.4:8a526ec, Jun  3 2025, 17:46:04) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #area and perimeter of rectangle
+>>> l=int(input('enter the length of rectangle'))
+enter the length of rectangle50
+>>> b=int(input('enter the breadth of rectangle'))
+enter the breadth of rectangle25
+>>> #calculating area and perimeter
+>>> area=l*b
+>>> perimeter=2*(l+b)
+>>> #displaying results
+>>> print('the area of rectangle is',area)
+the area of rectangle is 1250
+>>> print('the perimeter of rectangle is',perimeter)
+the perimeter of rectangle is 150
